12c009afb35e78d49aa92cc395b22f231c9fcdf5
Louis <louis.dubois@epitech.eu>
2026-07-30 18:39:03 +0200

    ADD: class Ship

---
+++ src/components/shipClass.js
@@ -0,0 +1,16 @@
+class Ship {
+    constructor(len, p1, p2) {
+        this.length = len;
+        this.hit = 0;
+        this.sunk = false;
+        this.coor = [p1, p2];
+    }
+
+    hit() {
+
+    }
+
+    isSunk() {
+        
+    }
+}
