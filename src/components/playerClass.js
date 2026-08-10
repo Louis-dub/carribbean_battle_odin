8fd6a34001f4875f8545696d9dbb6797cff9e087
Louis <louis.dubois@epitech.eu>
2026-08-10 20:11:32 +0200

    ADD: method isLose

--- src/components/playerClass.js
+++ src/components/playerClass.js
@@ -6,6 +6,21 @@ export class Player {
     constructor() {
         this.board = new GameBoard();
         this.gridDOM = createGrid("player", "Your Fleet", this.board);
+        this.lose = false;
+    }
+
+    isLose() {
+        let sunks = 0;
+        this.board.ships.forEach(ship => {
+            if (ship.isSunk())
+                sunks++;
+        })
+
+        if (sunks === 5) {
+            this.lose = true;
+            return true;
+        }
+        return false;
     }
 }
 
@@ -14,5 +29,20 @@ export class Computer {
         this.board = new GameBoard();
         this.gridDOM = createGrid("computer", "Enemy Fleet", this.board);
         this.board.ships = createShip();
+        this.lose = false;
+    }
+
+    isLose() {
+        let sunks = 0;
+        this.board.ships.forEach(ship => {
+            if (ship.isSunk())
+                sunks++;
+        })
+
+        if (sunks === 5) {
+            this.lose = true;
+            return true;
+        }
+        return false;
     }
 }
