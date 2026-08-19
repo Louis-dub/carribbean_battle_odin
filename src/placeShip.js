cf31d836b0470026bffbc3375ee9142be355a864
Louis <louis.dubois@epitech.eu>
2026-08-19 22:52:51 +0200

    ADD: select ship

--- src/placeShip.js
+++ src/placeShip.js
@@ -2,17 +2,14 @@ import { Ship } from "./components/shipClass.js";
 import { launchGame } from "./functions/createPlayer.js";
 
 const placeShip = document.createElement("div");
-
 placeShip.className = "place-ship";
 
 const grid = document.createElement("div");
-
-grid.className = "grid";
+grid.className = "grid-place";
 
 for (let i = 0; i < 12; i++) {
     for (let j = 0; j < 12; j++) {
         let caseGrid = document.createElement("div");
-
         caseGrid.className = "case";
         caseGrid.value = `${i} ${j}`;
         grid.appendChild(caseGrid);
@@ -20,15 +17,32 @@ for (let i = 0; i < 12; i++) {
 }
 
 const btnLauchGame = document.createElement("button");
-
 btnLauchGame.textContent = "Play";
-btnLauchGame.className = "launch-game"
+btnLauchGame.className = "launch-game";
 
 btnLauchGame.addEventListener("click", () => {
     launchGame(false);
 });
 
+const handleShip = document.createElement("div");
+handleShip.className = "handle-ship";
+
+const selectShip = document.createElement("select");
+selectShip.name = "ship";
+
+selectShip.innerHTML = `
+    <option value="">Choose a ship</option>
+    <option value="gunboat">Gunboat</option>
+    <option value="schooner">Schooner</option>
+    <option value="brick">Brick</option>
+    <option value="frigate">Frigate</option>
+    <option value="man-o-war">Man-o'-war</option>
+`;
+
+handleShip.appendChild(selectShip);
+
 placeShip.appendChild(grid);
+placeShip.appendChild(handleShip);
 placeShip.appendChild(btnLauchGame);
 
 export { placeShip };
