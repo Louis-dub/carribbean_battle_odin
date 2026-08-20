c7af4d75d127bed11dba096d321aa69a27547387
Louis <louis.dubois@epitech.eu>
2026-08-20 15:17:28 +0200

    ADD: display ship on grid to place ship

--- src/placeShip.js
+++ src/placeShip.js
@@ -1,6 +1,8 @@
 import { Ship } from "./components/shipClass.js";
 import { launchGame } from "./functions/createPlayer.js";
 
+const ships = [];
+
 const placeShip = document.createElement("div");
 placeShip.className = "place-ship";
 
@@ -32,12 +34,23 @@ selectShip.name = "ship";
 
 selectShip.innerHTML = `
     <option value="">Choose a ship</option>
-    <option value="gunboat">Gunboat</option>
-    <option value="schooner">Schooner</option>
-    <option value="brick">Brick</option>
-    <option value="frigate">Frigate</option>
-    <option value="man-o-war">Man-o'-war</option>
+    <option value="2">Gunboat</option>
+    <option value="3">Schooner</option>
+    <option value="4">Brick</option>
+    <option value="5">Frigate</option>
+    <option value="6">Man-o'-war</option>
 `;
+
+selectShip.addEventListener("change", () => {
+    Array.from(grid.children).forEach(c => c.className = "case");
+    let p = [0, 0];
+    let dir = [0, 1];
+
+    for (let i = 0; i < parseInt(selectShip.value); i++) {
+        Array.from(grid.children).find(c => c.value === `${p[0]} ${p[1]}`).className = "case ship-player";
+        p = [p[0] + dir[0], p[1] + dir[1]];
+    }
+});
 
 const btnPlaceShip = document.createElement("button");
 btnPlaceShip.className = "btn-place-ship";
