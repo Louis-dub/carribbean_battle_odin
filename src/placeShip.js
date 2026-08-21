6912ea6edf06b98d458f44614709f65885b1e117
Louis <louis.dubois@epitech.eu>
2026-08-21 19:50:13 +0200

    ADD: place ship

--- src/placeShip.js
+++ src/placeShip.js
@@ -1,4 +1,5 @@
 import { Ship } from "./components/shipClass.js";
+import { changeColourShip } from "./functions/changeColourShip.js";
 import { launchGame } from "./functions/createPlayer.js";
 import { moveShip, rotateShip } from "./functions/moveShip.js";
 
@@ -10,15 +11,24 @@ function handleKeydown(e, p, ogP, dir) {
     }
     ogP = moveShip(e, parseInt(selectShip.value), ogP, dir);
     p = [...ogP];
-    Array.from(grid.children).forEach(c => c.className = "case");
+    Array.from(grid.children).forEach(c => {if (!c.className.includes("touch")) c.className = "case"});
     for (let i = 0; i < parseInt(selectShip.value); i++) {
-        Array.from(grid.children).find(c => c.value === `${p[0]} ${p[1]}`).className = "case ship-player";
+        let currCase = Array.from(grid.children).find(c => c.value === `${p[0]} ${p[1]}` && !c.className.includes("touch"));
+        if (currCase)
+            currCase.className = "case ship-player";
         p = [p[0] + dir[0], p[1] + dir[1]];
     }
     return { p, ogP, dir};
 }
 
 const ships = [];
+const listPlaceShip = [false, false, false, false, false];
+let currVal = {
+    place: false,
+    ogP: [0, 0],
+    dir: [0, 1],
+    len: 2,
+}
 
 const placeShip = document.createElement("div");
 placeShip.className = "place-ship";
@@ -40,7 +50,10 @@ btnLauchGame.textContent = "Play";
 btnLauchGame.className = "launch-game";
 
 btnLauchGame.addEventListener("click", () => {
-    launchGame(false);
+    if (listPlaceShip.includes(false))
+        alert("Place all your ships");
+    else
+        launchGame(ships);
 });
 
 const handleShip = document.createElement("div");
@@ -59,39 +72,80 @@ selectShip.innerHTML = `
 `;
 
 selectShip.addEventListener("change", () => {
+    errorMessage.textContent = "";
+    if (selectShip.value !== "" && listPlaceShip[parseInt(selectShip.value) - 2]) {
+        Array.from(grid.children).forEach(c => {if (!c.className.includes("touch")) c.className = "case"});
+        document.activeElement.blur();
+        return;
+    }
     let p = [0, 0];
     let ogP = [0, 0];
     let dir = [0, 1];
+    let len = parseInt(selectShip.value);
     if (selectShip.value === "") {
         document.removeEventListener("keydown", (e) => {
-            const newVal = handleKeydown(e, p, ogP, dir);
-            p = newVal.p;
-            ogP = newVal.ogP;
-            dir = [...newVal.dir];
+            if (!listPlaceShip[parseInt(selectShip.value) - 2]) {
+                const newVal = handleKeydown(e, p, ogP, dir);
+                p = [...newVal.p];
+                ogP = [...newVal.ogP];
+                dir = [...newVal.dir];
+                currVal.ogP = [...ogP];
+                currVal.dir = [...dir];
+            }
         });
     } else {
         document.addEventListener("keydown", (e) => {
-            const newVal = handleKeydown(e, p, ogP, dir);
-            p = newVal.p;
-            ogP = newVal.ogP;
-            dir = [...newVal.dir];
+            if (!listPlaceShip[parseInt(selectShip.value) - 2]) {
+                const newVal = handleKeydown(e, p, ogP, dir);
+                p = [...newVal.p];
+                ogP = [...newVal.ogP];
+                dir = [...newVal.dir];
+                currVal.ogP = [...ogP];
+                currVal.dir = [...dir];
+            }
         });
     }
-    Array.from(grid.children).forEach(c => c.className = "case");
+    Array.from(grid.children).forEach(c => {if (!c.className.includes("touch")) c.className = "case"});
 
-    for (let i = 0; i < parseInt(selectShip.value); i++) {
-        Array.from(grid.children).find(c => c.value === `${p[0]} ${p[1]}`).className = "case ship-player";
+    for (let i = 0; i < len; i++) {
+        let currCase = Array.from(grid.children).find(c => c.value === `${p[0]} ${p[1]}` && !c.className.includes("touch"));
+        if (currCase)
+            currCase.className = "case ship-player";
         p = [p[0] + dir[0], p[1] + dir[1]];
+    }
+    currVal.place = true;
+    currVal.len = parseInt(selectShip.value);
+    if (selectShip.value === "") {
+        currVal.place = false;
+        currVal.len = 0;
     }
     document.activeElement.blur();
 });
+
+const errorMessage = document.createElement("p");
+errorMessage.className = "error-message";
 
 const btnPlaceShip = document.createElement("button");
 btnPlaceShip.className = "btn-place-ship";
 btnPlaceShip.textContent = "Place Ship";
 
+btnPlaceShip.addEventListener("click", () => {
+    if (!currVal.place) {
+        errorMessage.textContent = "Select a Ship";
+    } else if (!listPlaceShip[currVal.len - 2]) {
+        listPlaceShip[currVal.len - 2] = true;
+        let p = [...currVal.ogP];
+        for (let i = 0; i < currVal.len - 1; i++)
+            p = [p[0] + currVal.dir[0], p[1] + currVal.dir[1]];
+        ships.push(new Ship(currVal.len, currVal.ogP, p));
+        console.log(JSON.stringify(ships));
+        changeColourShip(grid.children, ships[ships.length - 1], "case touch");
+    }
+});
+
 handleShip.appendChild(selectShip);
 handleShip.appendChild(btnPlaceShip);
+handleShip.appendChild(errorMessage);
 
 placeShip.appendChild(grid);
 placeShip.appendChild(handleShip);
